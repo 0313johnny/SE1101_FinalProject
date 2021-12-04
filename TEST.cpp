74e73f3d20eca3b8bf68e3b04fd2c0a6a0988439
zxc90836 <73873264+zxc90836@users.noreply.github.com>
2021-12-04 21:35:26 +0800

    merge

--- TEST.cpp
+++ TEST.cpp
@@ -6,10 +6,7 @@ int main() {
     std::cout << "Hello World!";
     std::cout << "change";
     std::cout << "00857027";
-<<<<<<< HEAD
     std::cout << "7777";
-=======
     std::cout << "have material";
->>>>>>> 5c7d4c743052acbfb224769bd01b38e9eba0196f
     return 0;
 }
