9fe9a0c3d387537c05bb2ee192c6f0fd5fb25f2b
JerryTian26153 <95434328+JerryTian26153@users.noreply.github.com>
2021-12-13 23:49:52 +0800

    首頁更新
    新增:
    毛玻璃卡片測試
    字體調整

--- js/script.js
+++ js/script.js
@@ -6,23 +6,35 @@
             document.querySelector('#scroll-top').classList.remove('active');
         }
     }
-    menu.onclick = () =>{
-
-        menu.classList.toggle('fa-times');
-        navbar.classList.toggle('active');
-
-    }
-
-    window.onscroll = () =>{
-
-        menu.classList.remove('fa-times');
-        navbar.classList.remove('active');
-
-        if(window.scrollY > 60){
-            document.querySelector('#scroll-top').classList.add('active');
-        }else{
-            document.querySelector('#scroll-top').classList.remove('active');
-        }
-
-    }
 }
+$(document).ready(function () {
+    $("#maindiv .personal").click(function () { //card動畫設定
+        var textreg;
+        var h1reg;
+        reg = $(this).attr("src");
+        textreg=$(this).siblings().html();
+        h1reg=$(this).siblings().children("span").html();
+        setTimeout(function(){
+            $("div.display").trigger("mouseup");
+
+            $(".skr").stop();
+            $(".card").stop();
+
+            $(".card .cardheader .personal").attr("src", reg);
+
+            $(".skr").animate({
+                opacity: "0.4"
+            })
+            $(".card").toggle();
+            setTimeout(function () {
+                $(".card .cardbody").css("display", "");
+                $(".card .cardheader span").css("display", "");
+
+                //textreg=$(this).siblings("").html();
+                $(".card .cardheader span").html(textreg);
+                $(".card .cardbody h1").html(h1reg);
+                //console.log(h1reg);
+            }, 500);
+        },50);
+    });
+});
