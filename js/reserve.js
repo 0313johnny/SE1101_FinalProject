f6d13e53dee50a079c9018480c6e45ccdd8929ca
zxc90836 <0979710326a@gmail.com>
2021-12-25 21:58:36 +0800

    reserve
    添加預約

--- js/reserve.js
+++ js/reserve.js
@@ -3,7 +3,8 @@ $("document").ready(function(){
     $("#search_btn").click(function(){
         console.log("search");
         var appointINFO = {};
-        appointINFO.date = $("input[name='date']").val();
+        appointINFO.usingTime = {};
+        appointINFO.usingTime.date = $("input[name='date']").val();
         var start = parseInt($("select[name='start_class']").val());
         var period = parseInt($("select[name='period']").val());
         console.log(start +"    "+ period);
@@ -12,9 +13,22 @@ $("document").ready(function(){
         {
             arry[i] = start + i;
         }
-        appointINFO.time = arry;
-
+        appointINFO.usingTime.time = arry;
+        appointINFO.usingTime.class = period;
         console.log(appointINFO);
-    });
+        var data = JSON.stringify(appointINFO);
+        $.ajax({ 
+            type: "GET",
+            url: "http://127.0.0.1:5000/DB/findIdleClassroom", 
+            dataType: "json",
+            data:data,
+            success: function(re){
+                console.log(re);
+            },
+            error: function (thrownError) {
+                console.log(thrownError);
+              }
+        });
 
+    });
 });
